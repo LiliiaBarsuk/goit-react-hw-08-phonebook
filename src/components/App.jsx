ff1a40c42c6d60f61111d335f741ceb3aa720795
LiliiaBarsuk <lilya.b@live.com>
2022-10-07 11:31:32 +0300

    add save to local storage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,12 +6,7 @@ import { ContactList } from "./ContactList/ContactList";
 
 export class App extends Component {
   state = {
-    contacts: [
-      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-    ],
+    contacts: [],
     filter: '',
   }
   
@@ -46,6 +41,21 @@ export class App extends Component {
     this.setState({
       contacts: this.state.contacts.filter(contact => contact.id !== id)
     })
+  }
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+      
+    if (parsedContacts) {
+        this.setState({contacts: parsedContacts})
+      }  
+  }
+
+  componentDidUpdate(prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
+    }
   }
 
   render() {
