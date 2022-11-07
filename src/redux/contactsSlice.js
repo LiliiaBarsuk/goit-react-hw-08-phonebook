4c58cf595adff9698cdb272e2ada46e1effa635d
LiliiaBarsuk <98609786+LiliiaBarsuk@users.noreply.github.com>
2022-11-07 11:59:07 +0200

    Update contactsSlice.js

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -30,7 +30,7 @@ const contactsSlice = createSlice({
         },
         
         deleteContact: (state, action) => {
-          return state.contacts.filter(contact => contact.id !== action.payload);
+           state.contacts.filter(contact => contact.id !== action.payload);
         },
     }
 });
